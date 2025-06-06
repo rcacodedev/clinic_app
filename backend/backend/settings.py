ca50ba716672b4c5b3b335f14a89f52f7a5cbbdf
rcacodedev <rcacodedv@gmail.com>
2025-06-06 18:46:22 +0200

    Update 0.41v

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,6 +20,7 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Forzar redeploy a Render tras limpiar base de datos
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
